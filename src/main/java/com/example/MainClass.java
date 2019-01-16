0ba0398eb4f0de40df6993c1b0f0de0ff277756d
igorkorz <mr.randolph-carter@yandex.ru>
2019-01-16 13:33:37 +0300

    Added datasource selection

--- src/main/java/com/example/MainClass.java
+++ src/main/java/com/example/MainClass.java
@@ -1,18 +1,22 @@
 package com.example;
 
-import com.example.controller.DBProperties;
 import com.example.view.Menu;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
-import java.util.Map;
 
 public class MainClass {
   public static void main(String[] args) {
+    String contextFile;
+
+    switch (args[0]) {
+      case "file": contextFile = "filedictionary-context.xml"; break;
+      case "database": contextFile = "dbdictionary-context.xml"; break;
+      default: System.out.println("Invalid input parameter!"); return;
+    }
+
     ApplicationContext context = new ClassPathXmlApplicationContext(
-            "dictionary-context.xml");
+            contextFile);
     Menu consoleMenu = (Menu) context.getBean("menu");
     consoleMenu.run();
   }
