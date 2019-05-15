e454b660a37f70145c17c4d4eb0b8870aa863fa1
igorkorz <mr.randolph-carter@yandex.ru>
2019-05-15 17:16:15 +0300

    Fix constructor and Add exception handler

--- src/main/java/com/example/controller/DBDictionary.java
+++ src/main/java/com/example/controller/DBDictionary.java
@@ -2,8 +2,9 @@ package com.example.controller;
 
 import com.example.model.DictionaryRecord;
 import com.example.model.Property;
-import com.example.validation.DictionaryValidator;
+import com.example.validation.ErrorProperty;
 import com.example.validation.Validator;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,27 +14,39 @@ import java.util.List;
 public class DBDictionary implements Dictionary {
     private JdbcTemplate jdbcTemplate;
     private Validator validator;
+    private int keyLength;
+    private String keyRegex;
     private String name;
     private int type;
 
 
-    public DBDictionary(JdbcTemplate jdbcTemplate, int keyLength, String keySymbols, String name, int type) {
-            this.jdbcTemplate = jdbcTemplate;
-            this.name = name;
-            this.type = type;
-
-            initDictionary();
-
-            this.validator = new DictionaryValidator(jdbcTemplate, keyLength, keySymbols, type);
+    public DBDictionary(
+            JdbcTemplate jdbcTemplate,
+            Validator validator,
+            int keyLength,
+            String keyRegex,
+            String name,
+            int type
+            ) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.validator = validator;
+        this.keyLength = keyLength;
+        this.keyRegex = keyRegex;
+        this.name = name;
+        this.type = type;
     }
 
     @Override
     public synchronized List<Property> getDictionary() {
-        return jdbcTemplate.query(
-                "SELECT key, value FROM key k JOIN value v ON k.id = v.key_id WHERE type = ?",
-                recordMap(),
-                type
-        );
+        try {
+            return jdbcTemplate.query(
+                    "SELECT key, value FROM key k JOIN value v ON k.id = v.key_id WHERE type = ?",
+                    recordMap(),
+                    type
+            );
+        } catch (DataAccessException e) {
+            return Collections.singletonList(new ErrorProperty(e));
+        }
     }
 
     @Override
@@ -43,105 +56,122 @@ public class DBDictionary implements Dictionary {
 
     @Override
     public synchronized Property put(String key, String value) {
-        if (!validator.isValidRecord(key, value)) return validator.getResult();
+        try {
+            if (!validator.isValidRecord(key, value, keyLength, keyRegex)) return validator.getResult();
 
-        if (validator.keyExists(key)) {
-            if (validator.recordExists(key, value)) {
+            if (validator.keyExists(key, type)) {
+                if (validator.recordExists(key, value, type)) {
+                    jdbcTemplate.update(
+                            "UPDATE value SET value = ? WHERE key_id = get_key_id(?, ?)",
+                            value,
+                            key,
+                            type
+                    );
+                } else {
+                    insertIntoValueTable(key, value);
+                }
+            } else {
                 jdbcTemplate.update(
-                        "UPDATE value SET value = ? WHERE key_id = get_key_id(?, ?)",
-                        value,
+                        "INSERT INTO key (key, type) VALUES (?, ?)",
                         key,
                         type
                 );
-            } else {
+
                 insertIntoValueTable(key, value);
             }
-        } else {
-            jdbcTemplate.update(
-                    "INSERT INTO key (key, type) VALUES (?, ?)",
-                    key,
-                    type
-            );
 
-            insertIntoValueTable(key, value);
+            return new DictionaryRecord(key, value, type);
+        } catch (DataAccessException e) {
+            return new ErrorProperty(e);
         }
-
-        return new DictionaryRecord(key, value, type);
     }
 
     @Override
     public synchronized List<Property> get(String key) {
-        if (!validator.keyExists(key)) return Collections.singletonList(validator.getResult());
+        try {
+            if (!validator.keyExists(key, type)) return Collections.singletonList(validator.getResult());
 
-        return jdbcTemplate.query(
-                "SELECT * FROM key k JOIN value v ON k.id = v.key_id WHERE key = ?",
-                recordMap(),
-                key
-        );
+            return jdbcTemplate.query(
+                    "SELECT * FROM key k JOIN value v ON k.id = v.key_id WHERE key = ?",
+                    recordMap(),
+                    key
+            );
+        } catch (DataAccessException e) {
+            return Collections.singletonList(new ErrorProperty(e));
+        }
     }
 
     @Override
     public synchronized Property remove(String key, String value) {
-        if (!validator.keyExists(key)) return validator.getResult();
+        try {
+            if (!validator.keyExists(key, type)) return validator.getResult();
 
-        if (!validator.recordExists(key, value)) return validator.getResult();
+            if (!validator.recordExists(key, value, type)) return validator.getResult();
 
-        jdbcTemplate.update(
-                "DELETE FROM value WHERE key_id = get_key_id(?, ?) AND value = ?",
-                key,
-                type,
-                value
-        );
+            jdbcTemplate.update(
+                    "DELETE FROM value WHERE key_id = get_key_id(?, ?) AND value = ?",
+                    key,
+                    type,
+                    value
+            );
 
-        return new DictionaryRecord(key, value, type);
+            return new DictionaryRecord(key, value, type);
+        } catch (DataAccessException e) {
+            return new ErrorProperty(e);
+        }
     }
 
-    private void initDictionary() {
-        jdbcTemplate.execute(
-                "CREATE TABLE IF NOT EXISTS key" +
-                        "(" +
-                        "id BIGSERIAL NOT NULL," +
-                        "key VARCHAR(255) NOT NULL," +
-                        "type INTEGER NOT NULL," +
-                        "CONSTRAINT unique_id PRIMARY KEY (id)," +
-                        "CONSTRAINT dict_key UNIQUE (key, type)" +
-                        ")"
-        );
-        jdbcTemplate.execute(
-                "CREATE TABLE IF NOT EXISTS value" +
-                        "(" +
-                        "id BIGSERIAL NOT NULL," +
-                        "value VARCHAR(255) NOT NULL," +
-                        "key_id BIGINT," +
-                        "CONSTRAINT unique_val_id PRIMARY KEY (id)," +
-                        "CONSTRAINT property UNIQUE (key_id, value)," +
-                        "CONSTRAINT key_id FOREIGN KEY (key_id)" +
-                        "   REFERENCES key (id) MATCH SIMPLE" +
-                        "   ON UPDATE NO ACTION" +
-                        "   ON DELETE CASCADE" +
-                        ")"
-        );
-        jdbcTemplate.execute(
-                "CREATE OR REPLACE FUNCTION key_exists(k varchar, t integer) RETURNS boolean AS $$" +
-                        "BEGIN " +
-                        "RETURN EXISTS(SELECT 1 FROM key WHERE key = k AND type = t);" +
-                        "END" +
-                        "$$ LANGUAGE PLPGSQL"
-        );
-        jdbcTemplate.execute(
-                "CREATE OR REPLACE FUNCTION record_exists(k varchar, v varchar, t integer) RETURNS boolean AS $$" +
-                        "BEGIN " +
-                        "RETURN EXISTS(SELECT 1 FROM key JOIN value ON key.id = key_id WHERE key = k AND value = v AND type = t);" +
-                        "END" +
-                        "$$ LANGUAGE PLPGSQL"
-        );
-        jdbcTemplate.execute(
-                "CREATE OR REPLACE FUNCTION get_key_id(k varchar, t integer) RETURNS integer AS $$" +
-                        "BEGIN " +
-                        "RETURN id FROM key WHERE key = k AND type = t;" +
-                        "END" +
-                        "$$ LANGUAGE PLPGSQL"
-        );
+    @Override
+    public void initDictionary() {
+        try {
+            jdbcTemplate.execute(
+                    "CREATE TABLE IF NOT EXISTS key" +
+                            "(" +
+                            "id BIGSERIAL NOT NULL," +
+                            "key VARCHAR(255) NOT NULL," +
+                            "type INTEGER NOT NULL," +
+                            "CONSTRAINT unique_id PRIMARY KEY (id)," +
+                            "CONSTRAINT dict_key UNIQUE (key, type)" +
+                            ")"
+            );
+            jdbcTemplate.execute(
+                    "CREATE TABLE IF NOT EXISTS value" +
+                            "(" +
+                            "id BIGSERIAL NOT NULL," +
+                            "value VARCHAR(255) NOT NULL," +
+                            "key_id BIGINT," +
+                            "CONSTRAINT unique_val_id PRIMARY KEY (id)," +
+                            "CONSTRAINT property UNIQUE (key_id, value)," +
+                            "CONSTRAINT key_id FOREIGN KEY (key_id)" +
+                            "   REFERENCES key (id) MATCH SIMPLE" +
+                            "   ON UPDATE NO ACTION" +
+                            "   ON DELETE CASCADE" +
+                            ")"
+            );
+            jdbcTemplate.execute(
+                    "CREATE OR REPLACE FUNCTION key_exists(k varchar, t integer) RETURNS boolean AS $$" +
+                            "BEGIN " +
+                            "RETURN EXISTS(SELECT 1 FROM key WHERE key = k AND type = t);" +
+                            "END" +
+                            "$$ LANGUAGE PLPGSQL"
+            );
+            jdbcTemplate.execute(
+                    "CREATE OR REPLACE FUNCTION record_exists(k varchar, v varchar, t integer) RETURNS boolean AS $$" +
+                            "BEGIN " +
+                            "RETURN EXISTS(SELECT 1 FROM key JOIN value ON key.id = key_id WHERE key = k AND value = v AND type = t);" +
+                            "END" +
+                            "$$ LANGUAGE PLPGSQL"
+            );
+            jdbcTemplate.execute(
+                    "CREATE OR REPLACE FUNCTION get_key_id(k varchar, t integer) RETURNS integer AS $$" +
+                            "BEGIN " +
+                            "RETURN (SELECT id FROM key WHERE key = k AND type = t);" +
+                            "END" +
+                            "$$ LANGUAGE PLPGSQL"
+            );
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     private RowMapper<Property> recordMap() {
@@ -155,7 +185,7 @@ public class DBDictionary implements Dictionary {
         };
     }
 
-    private void insertIntoValueTable(String key, String value) {
+    private void insertIntoValueTable(String key, String value) throws DataAccessException {
         jdbcTemplate.update("INSERT INTO value (value, key_id) VALUES (?, get_key_id(?, ?))", value, key, type);
     }
 }
