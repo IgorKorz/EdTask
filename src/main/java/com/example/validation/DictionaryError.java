ca7d4ad59ac71200f2e6f5c486decedc3b8da977
igorkorz <mr.randolph-carter@yandex.ru>
2019-05-15 14:10:53 +0300

    Move PropertyError in DictionaryError

--- src/main/java/com/example/validation/DictionaryError.java
+++ src/main/java/com/example/validation/DictionaryError.java
@@ -1,27 +1,69 @@
 package com.example.validation;
 
+import com.example.model.Property;
+
 public enum DictionaryError {
-    Empty("DictionaryError", "Empty result!"),
-    EmptyKey("Invalid key", "Empty key!"),
-    EmptyValue("Invalid value", "Empty value!"),
-    KeyTooLong("Invalid key", "Key too long!"),
-    KeyTooShort("Invalid key", "Key too short!"),
-    KeyNotMatch("Invalid key", "Key not match!"),
-    RecordNotFound("Record error", "Record not found!");
+    Empty(new ErrorProperty("DictionaryError", "Empty result!")),
+    EmptyKey(new ErrorProperty("Invalid key", "Empty key!")),
+    EmptyValue(new ErrorProperty("Invalid value", "Empty value!")),
+    KeyTooLong(new ErrorProperty("Invalid key", "Key too long!")),
+    KeyTooShort(new ErrorProperty("Invalid key", "Key too short!")),
+    KeyNotFound(new ErrorProperty("Key error", "Key not found")),
+    KeyNotMatch(new ErrorProperty("Invalid key", "Key not match!")),
+    RecordNotFound(new ErrorProperty("Record error", "Record not found!"));
 
-    private String error;
-    private String message;
+    Property error;
 
-    DictionaryError(String error, String message) {
+    DictionaryError(Property error) {
         this.error = error;
-        this.message = message;
     }
 
-    public String getError() {
+    public Property getError() {
         return error;
     }
 
-    public String getMessage() {
-        return message;
+    private static class ErrorProperty implements Property {
+        private String error;
+        private String message;
+
+        ErrorProperty(String error, String message) {
+            this.error = error;
+            this.message = message;
+        }
+
+        @Override
+        public String getKey() {
+            return error;
+        }
+
+        @Override
+        public void setKey(String error) {
+            this.error = error;
+        }
+
+        @Override
+        public String getValue() {
+            return message;
+        }
+
+        @Override
+        public void setValue(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public int getType() {
+            return -1;
+        }
+
+        @Override
+        public void setType(int type) {
+
+        }
+
+        @Override
+        public String toString() {
+            return error + ": " + message;
+        }
     }
 }
