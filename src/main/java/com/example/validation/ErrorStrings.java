f9edfc96be7316f97f737a53b82f1dede8061629
igorkorz <mr.randolph-carter@yandex.ru>
2019-05-16 11:17:38 +0300

    Move dictionary strings of error to validation

---
+++ src/main/java/com/example/validation/ErrorStrings.java
@@ -0,0 +1,17 @@
+package com.example.validation;
+
+public final class ErrorStrings {
+    public static final String DICTIONARY_ERROR = "DictionaryError";
+    public static final String KEY_ERROR = "Key error";
+    public static final String RECORD_ERROR = "Record error";
+    public static final String INVALID_KEY = "Invalid key";
+    public static final String INVALID_VALUE = "Invalid value";
+    public static final String EMPTY_RESULT = "Empty result!";
+    public static final String EMPTY_VALUE = "Empty value!";
+    public static final String EMPTY_KEY = "Empty key!";
+    public static final String KEY_TOO_LONG = "Key too long!";
+    public static final String KEY_TOO_SHORT = "Key too short!";
+    public static final String KEY_NOT_FOUND = "Key not found!";
+    public static final String KEY_NOT_MATCH = "Key not match!";
+    public static final String RECORD_NOT_FOUND = "Record not found!";
+}
